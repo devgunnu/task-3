8505982bcf6aecf71e0d8038c911ea046314b6f0
Gunbir Singh <139300251+devgunnu@users.noreply.github.com>
2023-08-06 19:25:31 +0530

    Add files via upload

---
+++ file extension.py
@@ -0,0 +1,3 @@
+file=input("Input the File Name")
+extension=file.split(".")
+print("The extension of the file is:",repr(extension[-1]))
---
+++ radius of circle.py
@@ -0,0 +1,4 @@
+import math
+radius=float(input("Input the radius of the circle"))
+area=(radius**2)*math.pi
+print(area)
